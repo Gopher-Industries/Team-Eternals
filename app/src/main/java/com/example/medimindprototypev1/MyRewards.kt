5c932dbb93b85209f6488dcd4874d5a4043b3768
Kishen Mayadunne <kmayadunne@deakin.edu.au>
2022-05-18 10:26:57 +1000

    Test commit with a few changes to check for any errors during the Git Push > Added onClickListener to display a Toast when the buttons in My Rewards are clicked > Added a sample code as comment to use when these pages are created so they can be linked > Changed the value of the progress bar

--- app/src/main/java/com/example/medimindprototypev1/MyRewards.kt
+++ app/src/main/java/com/example/medimindprototypev1/MyRewards.kt
@@ -1,7 +1,10 @@
 package com.example.medimindprototypev1
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class MyRewards : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,5 +14,24 @@ class MyRewards : AppCompatActivity() {
         val actionBar = supportActionBar
         actionBar!!.title = "My Rewards"
         actionBar.setDisplayHomeAsUpEnabled(true)
+
+        //        The code below is used to set an onClickListener using the latest version of Kotlin
+        val myWishList: Button = findViewById (R.id.my_wish_list)
+        myWishList.setOnClickListener() {
+            Toast.makeText(this, "My Wish List Button was Clicked", Toast.LENGTH_LONG).show()
+//           ###### Use this code below to go to the desired page when the button is clicked #####
+//            val intent = Intent(this, Add the My Wish List Class Here::class.java)
+//            startActivity(intent)
+        }
+
+        val allRewards: Button = findViewById (R.id.all_rewards)
+        allRewards.setOnClickListener() {
+            Toast.makeText(this, "All Rewards Button was Clicked", Toast.LENGTH_LONG).show()
+        }
+
+        val claimRewards: Button = findViewById (R.id.claim_rewards)
+        claimRewards.setOnClickListener() {
+            Toast.makeText(this, "My Claimed Rewards Button was Clicked", Toast.LENGTH_LONG).show()
+        }
     }
 }
