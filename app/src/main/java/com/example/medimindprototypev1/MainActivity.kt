4dd7d86ed5d195115fc3db5f1ca2d2f17404e3ae
Kishen Mayadunne <kmayadunne@deakin.edu.au>
2022-05-25 20:51:57 +1000

    Major changes to the Home page (activity_main.xml), as it did not have a button class > Added a ImageButton class to make the image clickable and act as a replacement for the button to all the buttons > Changes the size of the images to fit the button size
    Added in the code for OnClick events in the MainActivity.kt file for all 4 buttons.
    > Added a toast for the two buttons that do not have a dedicated page

--- app/src/main/java/com/example/medimindprototypev1/MainActivity.kt
+++ app/src/main/java/com/example/medimindprototypev1/MainActivity.kt
@@ -4,7 +4,9 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageButton
 import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,16 +19,30 @@ class MainActivity : AppCompatActivity() {
         actionBar!!.title = "Home"
 
 //        The code below is used to set an onClickListener using the latest version of Kotlin
-        val myRewardsBtn: Button = findViewById (R.id.myRewardsBtn)
+        // Code for My Medications Button
+        val myMedicationBtn: ImageButton = findViewById (R.id.my_medications_btn)
+        myMedicationBtn.setOnClickListener() {
+            val intent = Intent(this, MyMedication::class.java)
+            startActivity(intent)
+        }
+
+        // Code for My Rewards Button
+        val myRewardsBtn: ImageButton = findViewById (R.id.my_rewards_btn)
         myRewardsBtn.setOnClickListener() {
             val intent = Intent(this, MyRewards::class.java)
             startActivity(intent)
         }
 
-        val myMedicationBtn: Button = findViewById (R.id.myMedicationBtn)
-        myMedicationBtn.setOnClickListener() {
-            val intent = Intent(this, MyMedication::class.java)
-            startActivity(intent)
+        // Code for My Info Button
+        val myInfoBtn: ImageButton = findViewById (R.id.my_info_btn)
+        myInfoBtn.setOnClickListener() {
+            Toast.makeText(this, "My Info Button was Clicked", Toast.LENGTH_LONG).show()
+        }
+
+        // Code for My Report Button
+        val myReportsBtn: ImageButton = findViewById (R.id.my_reports_btn)
+        myReportsBtn.setOnClickListener() {
+            Toast.makeText(this, "My Reports Button was Clicked", Toast.LENGTH_LONG).show()
         }
     }
 }
