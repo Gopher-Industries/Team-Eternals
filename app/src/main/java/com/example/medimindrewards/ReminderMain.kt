a83deae4d4c04fa261ec3e1db1c9d91175463c44
wonster <wooonster@outlook.com>
2022-12-02 14:48:48 +1100

    Added basic buttons to Reminder Home (Wonster)

--- app/src/main/java/com/example/medimindrewards/ReminderMain.kt
+++ app/src/main/java/com/example/medimindrewards/ReminderMain.kt
@@ -5,8 +5,29 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.example.medimindrewards.databinding.FragmentReminderBinding
 
 class ReminderMain : Fragment() {
+    private var _binding : FragmentReminderBinding? = null
+    private val binding get() = _binding!!
 
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentReminderBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.backButtonReminderHome.setOnClickListener {
+            findNavController().navigate(R.id.action_ReminderMain_to_HomeNavigation)
+        }
+        binding.homeButtonReminderHome.setOnClickListener {
+            findNavController().navigate(R.id.action_ReminderMain_to_HomeNavigation)
+        }
+    }
 }
