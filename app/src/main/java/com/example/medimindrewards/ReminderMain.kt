2f443a46a819bd9e9b84790798c8da7eba5f1f21
wonster <wooonster@outlook.com>
2022-12-17 02:26:35 +1100

    Finish CreateNewReminderItem.kt and new changes to make new-reminder-create-card (Wonster, Yunjia)

--- app/src/main/java/com/example/medimindrewards/ReminderMain.kt
+++ app/src/main/java/com/example/medimindrewards/ReminderMain.kt
@@ -1,9 +1,12 @@
 package com.example.medimindrewards
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.example.medimindrewards.databinding.FragmentReminderBinding
